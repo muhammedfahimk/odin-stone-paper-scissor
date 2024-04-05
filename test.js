b537e2164dca9877010aac508dbae90163d3d30f
fahimktdunofficial@gmail.com <fahimktdunofficial@gmail.com>
2024-04-05 11:31:07 +0530

    First commit

---
+++ test.js
@@ -0,0 +1,59 @@
+
+function computerChoice() {
+    let choice = ['rock', 'papper', 'scissor']
+    return choice[Math.floor(Math.random() * choice.length)];
+}
+function playgame() {
+    w = 0;
+    l = 0;
+    t = 0;
+    result = "";
+    for (let i = 0; i < 5; i++) {
+        let computerCh = computerChoice();
+        let playerchoice = prompt("Enter Your Try").toLowerCase();
+        if (playerchoice === 'rock' || playerchoice === 'papper' || playerchoice === 'scissor') {
+            result = playround(computerCh, playerchoice)
+            console.log(result)
+            if (result === "Player win") {
+                w++;
+
+            }
+            else if (result === "Computer win") {
+                l++;
+
+
+            }
+            else if (result === "Tie") {
+                t++;
+            }
+        }
+    }
+    if (w > l) {
+        console.log("You win")
+    }
+    else if (l > w) {
+        console.log("You Lose")
+    }
+    else if (w == l) {
+        console.log("Match Tie")
+    }
+}
+playgame();
+function playround(computerCh, playerchoice) {
+    if (playerchoice == computerCh) {
+        return ("Tie")
+    }
+    else if (playerchoice == 'rock' && computerCh == 'scissor') {
+        return ("Player win")
+    }
+    else if (playerchoice == 'papper' && computerCh == 'rock') {
+        return ("Player win")
+    }
+    else if (playerchoice == 'scissor' && computerCh == 'papper') {
+        return ("Player win")
+    }
+    else {
+        return ("Computer win")
+    }
+}
+
